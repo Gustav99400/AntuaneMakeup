b6afd2875be178b65ebc110d168903f8babaaa81
Gustav99400 <gfloresv@ulasalle.edu.pe>
2025-04-02 18:00:30 -0500

    Primer commit

---
+++ js/firebaseConfig.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBGrcMYvWaYAzG5uefsq2srJeC5ihH23tg",
+    authDomain: "antuanemakeup-7950c.firebaseapp.com",
+    databaseURL: "https://antuanemakeup-7950c-default-rtdb.firebaseio.com",
+    projectId: "antuanemakeup-7950c",
+    storageBucket: "antuanemakeup-7950c.firebasestorage.app",
+    messagingSenderId: "653808803500",
+    appId: "1:653808803500:web:d579602ceffb1f6f144909",
+    measurementId: "G-7V5LN9QVJJ"
+  };
+
+
+const app = firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
